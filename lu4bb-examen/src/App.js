a714efec65d35b8baf89d988a434fc06a0391341
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-18 18:00:41 -0300

    padding home text

--- lu4bb-examen/src/App.js
+++ lu4bb-examen/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import NavBar from './component/NavBar';
+import './app.css';
 
 const App = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <div className="home-text">
+        <h2>LU4BB Training Center</h2>
+        <p>
+          Plataforma de Training para ingreso a categoría Novicio, ascenso a categoría General y Superior.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default App;
