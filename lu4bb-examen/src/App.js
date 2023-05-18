0b35c6a66bba7b6590c2cdc952b630f0c367e19c
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-18 18:03:43 -0300

    add logo

--- lu4bb-examen/src/App.js
+++ lu4bb-examen/src/App.js
@@ -6,11 +6,14 @@ const App = () => {
   return (
     <>
       <NavBar />
-      <div className="home-text">
-        <h2>LU4BB Training Center</h2>
-        <p>
-          Plataforma de Training para ingreso a categoría Novicio, ascenso a categoría General y Superior.
-        </p>
+      <div className="home-content">
+        <img src="/lu4bb-logo.jpg" alt="LU4BB Logo" className="logo" />
+        <div className="home-text">
+          <h2>LU4BB Training Center</h2>
+          <p>
+            Plataforma de Training para ingreso a categoría Novicio, ascenso a categoría General y Superior.
+          </p>
+        </div>
       </div>
     </>
   );
