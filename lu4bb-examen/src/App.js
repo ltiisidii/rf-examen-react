fc061dec52ba381521d74e4385fc49f7abb0c334
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-18 17:42:51 -0300

    rework

--- lu4bb-examen/src/App.js
+++ lu4bb-examen/src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
-import Home from './component/Home';
-import Quiz from './component/Quiz';
-import Learn from './component/Learn';
-import Footer from './component/Footer';
+import NavBar from './component/NavBar';
 
-function App() {
-  
-  return (
-    <div className="App">  
-      <Routes component="span">
-        <Route path="/" element={<Home />} />
-        <Route path="/learn" element={<Learn />} />
-          <Route path="/quiz" element={<Quiz />} />
-      </Routes>
-      <Footer />
-</div>
-)
-}
+const App = () => {
+  return <NavBar />;
+};
 
-export default App
+export default App;
---
+++ lu4bb-examen/src/component/NavBar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import { Home } from '@mui/icons-material';
+
+const NavBar = () => {
+  const [novicioMenuAnchor, setNovicioMenuAnchor] = React.useState(null);
+
+  const openNovicioMenu = (event) => {
+    setNovicioMenuAnchor(event.currentTarget);
+  };
+
+  const closeNovicioMenu = () => {
+    setNovicioMenuAnchor(null);
+  };
+
+  return (
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Box sx={{ mr: 1 }}>
+            <IconButton size="large" color="inherit">
+              <Home />
+            </IconButton>
+          </Box>
+          <Typography variant="h6" component="h1" noWrap sx={{ flexGrow: 1 }}>
+            Buenos Aires Radio Club
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit" onClick={openNovicioMenu}>
+            Novicio
+          </Button>
+          <Button color="inherit">Contacto</Button>
+          <Menu
+            anchorEl={novicioMenuAnchor}
+            open={Boolean(novicioMenuAnchor)}
+            onClose={closeNovicioMenu}
+          >
+            <MenuItem onClick={closeNovicioMenu}>Simulador</MenuItem>
+            <MenuItem onClick={closeNovicioMenu}>Info Técnica</MenuItem>
+            <MenuItem onClick={closeNovicioMenu}>Info Reglamentación</MenuItem>
+          </Menu>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
