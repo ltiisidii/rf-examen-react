73e380ba9acc2717c8e472907991af7cfa768a4a
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-18 22:12:55 -0300

    simulador padding

--- lu4bb-examen/src/component/SimuladorNovicio.js
+++ lu4bb-examen/src/component/SimuladorNovicio.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SimuladorNovicio = () => {
   return (
     <div>
-      <h2>Simulador Novicio</h2>
+      <h1 style={{ paddingTop: '50px' }}>Simulador para categoria Novicio</h1>
+      <h2 style={{ paddingTop: '50px' }}>Técnica, Reglamentación y Ética Operativa</h2>
+      <h2 style={{ paddingTop: '50px' }}>Too soon.... </h2>
       {/* Contenido del simulador de novicio */}
     </div>
   );
