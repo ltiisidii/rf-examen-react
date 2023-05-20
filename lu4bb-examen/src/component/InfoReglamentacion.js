7d2e685280ce70eb7a22953d935161af0fa0582d
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-19 21:58:22 -0300

    css info tecnica y reglamentacion

--- lu4bb-examen/src/component/InfoReglamentacion.js
+++ lu4bb-examen/src/component/InfoReglamentacion.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import facts from '../sources/facts-novicio-reglamentacion.json';
+import './InfoReglamentacion.css';
 
 const InfoReglamentacion = () => {
   const handlePrint = () => {
@@ -7,17 +8,19 @@ const InfoReglamentacion = () => {
   };
 
   return (
-    <div>
-      <h1 style={{ paddingTop: '50px' }}>Preguntas de examen: Categoría Novicio Reglamentación y Ética Operativa</h1>
-      <button onClick={handlePrint}>Imprimir</button> {/* Agregar botón de impresión */}
-      {facts.map((fact, index) => (
-        <div key={fact.id}>
-          {index > 0 && <hr />} {/* Separador visible */}
-          <p style={{ color: 'red', fontSize: '16px', fontWeight: 'bold' }}>{fact.id}</p> {/* Mostrar el ID */}
-          <h3>{fact.title}</h3>
-          <p style={{ color: 'green', fontSize: '16px', fontWeight: 'bold' }}>{fact.content}</p>
-        </div>
-      ))}
+    <div className="container">
+      <div className="info-tecnica-container">
+        <h1 style={{ paddingTop: '50px' }}>Preguntas de examen: Categoría Novicio Reglamentación y Ética Operativa</h1>
+        <button onClick={handlePrint}>Imprimir</button> {/* Agregar botón de impresión */}
+        {facts.map((fact, index) => (
+          <div key={fact.id}>
+            {index > 0 && <hr />} {/* Separador visible */}
+            <p style={{ color: 'red', fontSize: '16px', fontWeight: 'bold' }}>{fact.id}</p> {/* Mostrar el ID */}
+            <h3>{fact.title}</h3>
+            <p style={{ color: 'green', fontSize: '16px', fontWeight: 'bold' }}>{fact.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- lu4bb-examen/src/component/InfoTecnica.js
+++ lu4bb-examen/src/component/InfoTecnica.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import facts from '../sources/facts-novicio-tecnica.json';
+import './InfoTecnica.css';
 
 const InfoTecnica = () => {
   const handlePrint = () => {
@@ -7,17 +8,19 @@ const InfoTecnica = () => {
   };
 
   return (
-    <div>
-      <h1 style={{ paddingTop: '50px' }}>Preguntas de examen: Categoría Novicio Técnica</h1>
-      <button onClick={handlePrint}>Imprimir</button> {/* Agregar botón de impresión */}
-      {facts.map((fact, index) => (
-        <div key={fact.id}>
-          {index > 0 && <hr />} {/* Separador visible */}
-          <p style={{ color: 'red', fontSize: '16px', fontWeight: 'bold' }}>{fact.id}</p> {/* Mostrar el ID */}
-          <h3>{fact.title}</h3>
-          <p style={{ color: 'green', fontSize: '16px', fontWeight: 'bold' }}>{fact.content}</p>
-        </div>
-      ))}
+    <div className="container">
+      <div className="info-tecnica-container">
+        <h1 style={{ paddingTop: '50px' }}>Preguntas de examen: Categoría Novicio Técnica</h1>
+        <button onClick={handlePrint}>Imprimir</button> {/* Agregar botón de impresión */}
+        {facts.map((fact, index) => (
+          <div key={fact.id} className="fact-row">
+            {index > 0 && <hr />} {/* Separador visible */}
+            <p style={{ color: 'red', fontSize: '16px', fontWeight: 'bold' }}>{fact.id}</p> {/* Mostrar el ID */}
+            <h3>{fact.title}</h3>
+            <p style={{ color: 'green', fontSize: '16px', fontWeight: 'bold' }}>{fact.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
