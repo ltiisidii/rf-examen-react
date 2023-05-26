e3a22c7a26597933475b91c4c4521c52293f9780
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-26 18:31:14 -0300

    morse sim quitar boton verificar

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # React Examen Novicio
+
--- rf-examen/src/component/MorseSimulator.js
+++ rf-examen/src/component/MorseSimulator.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@chakra-ui/react";
-import wordsData from '../sources/wordsData.json'; // Ruta al archivo JSON
-import './MorseSimulator.css'; // Importar el archivo CSS
+import wordsData from '../sources/wordsData.json';
+import './MorseSimulator.css';
 
 const MorseSimulator = () => {
   const [morseWords, setMorseWords] = useState([]);
   const [userInputs, setUserInputs] = useState([]);
   const [isExamCompleted, setIsExamCompleted] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(new Array(25).fill(false));
 
   useEffect(() => {
-    // Obtener palabras aleatorias al cargar la página
     const randomIndices = getRandomIndices(wordsData.length, 25);
     const randomWords = randomIndices.map((index) => wordsData[index].word);
     setMorseWords(randomWords);
@@ -18,7 +18,6 @@ const MorseSimulator = () => {
   }, []);
 
   useEffect(() => {
-    // Verificar si todas las respuestas son correctas
     const isAllCorrect = userInputs.every((input, index) => input.trim() === morseWords[index]);
     setIsExamCompleted(isAllCorrect);
   }, [userInputs, morseWords]);
@@ -38,20 +37,11 @@ const MorseSimulator = () => {
     const newInputs = [...userInputs];
     newInputs[index] = event.target.value;
     setUserInputs(newInputs);
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const isAllCorrect = userInputs.every((input, index) => input.trim() === morseWords[index]);
-
-    if (isAllCorrect) {
-      setIsModalOpen(true);
-      setIsExamCompleted(true);
-    } else {
-      setIsModalOpen(false);
-      setIsExamCompleted(false);
-    }
+    const isCorrect = event.target.value.trim() === wordsData[index].word;
+    const newAnswerCorrect = [...isAnswerCorrect];
+    newAnswerCorrect[index] = isCorrect;
+    setIsAnswerCorrect(newAnswerCorrect);
   };
 
   const handleCloseModal = () => {
@@ -59,17 +49,18 @@ const MorseSimulator = () => {
   };
 
   return (
-    <div className="container"> {/* Agrega el contenedor */}
+    <div className="container">
       <div>
         <h1>Morse Simulator</h1>
         {morseWords.map((word, index) => (
           <div key={index}>
             <p>Ingresa el código Morse para la siguiente palabra:</p>
             <p>{word}</p>
-            <form onSubmit={handleSubmit}>
+            <form>
               <input type="text" value={userInputs[index]} onChange={(event) => handleInputChange(event, index)} />
-              <button type="submit">Verificar</button>
             </form>
+            {isAnswerCorrect[index] && <p className="correct-answer">Respuesta correcta</p>}
+            {!isAnswerCorrect[index] && userInputs[index].trim() !== '' && <p className="incorrect-answer">Respuesta incorrecta</p>}
             {isExamCompleted && index === morseWords.length - 1 && (
               <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
                 <ModalOverlay />
