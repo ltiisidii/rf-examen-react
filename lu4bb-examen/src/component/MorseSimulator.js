fcc7f3fdb2bdbe2e49f9954855106c4e1465fa80
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-26 15:02:38 -0300

    Morse sim

--- lu4bb-examen/src/component/MorseSimulator.js
+++ lu4bb-examen/src/component/MorseSimulator.js
@@ -1,53 +1,91 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@chakra-ui/react";
 import wordsData from '../sources/wordsData.json'; // Ruta al archivo JSON
 import './MorseSimulator.css'; // Importar el archivo CSS
 
 const MorseSimulator = () => {
-  const [morseWord, setMorseWord] = useState('');
-  const [userInput, setUserInput] = useState('');
+  const [morseWords, setMorseWords] = useState([]);
+  const [userInputs, setUserInputs] = useState([]);
+  const [isExamCompleted, setIsExamCompleted] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    // Seleccionar una palabra aleatoria al cargar la página
-    const randomIndex = Math.floor(Math.random() * wordsData.length);
-    const randomWord = wordsData[randomIndex].word;
-    setMorseWord(randomWord);
+    // Obtener palabras aleatorias al cargar la página
+    const randomIndices = getRandomIndices(wordsData.length, 25);
+    const randomWords = randomIndices.map((index) => wordsData[index].word);
+    setMorseWords(randomWords);
+    setUserInputs(new Array(25).fill(''));
   }, []);
 
-  const handleInputChange = (event) => {
-    setUserInput(event.target.value);
+  useEffect(() => {
+    // Verificar si todas las respuestas son correctas
+    const isAllCorrect = userInputs.every((input, index) => input.trim() === morseWords[index]);
+    setIsExamCompleted(isAllCorrect);
+  }, [userInputs, morseWords]);
+
+  const getRandomIndices = (max, count) => {
+    const indices = [];
+    while (indices.length < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      if (!indices.includes(randomIndex)) {
+        indices.push(randomIndex);
+      }
+    }
+    return indices;
+  };
+
+  const handleInputChange = (event, index) => {
+    const newInputs = [...userInputs];
+    newInputs[index] = event.target.value;
+    setUserInputs(newInputs);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const inputMorse = userInput.trim();
+    const isAllCorrect = userInputs.every((input, index) => input.trim() === morseWords[index]);
 
-    if (inputMorse === morseWord) {
-      // Respuesta correcta
-      alert('¡Respuesta correcta!');
+    if (isAllCorrect) {
+      setIsModalOpen(true);
+      setIsExamCompleted(true);
     } else {
-      // Respuesta incorrecta
-      alert('Respuesta incorrecta');
+      setIsModalOpen(false);
+      setIsExamCompleted(false);
     }
+  };
 
-    // Seleccionar una palabra aleatoria del arreglo
-    const randomIndex = Math.floor(Math.random() * wordsData.length);
-    const randomWord = wordsData[randomIndex].word;
-
-    setMorseWord(randomWord);
-    setUserInput('');
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <div className="container"> {/* Agrega el contenedor */}
       <div>
         <h1>Morse Simulator</h1>
-        <p>Ingresa el código Morse para la siguiente palabra:</p>
-        <p>{morseWord}</p>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={userInput} onChange={handleInputChange} />
-          <button type="submit">Verificar</button>
-        </form>
+        {morseWords.map((word, index) => (
+          <div key={index}>
+            <p>Ingresa el código Morse para la siguiente palabra:</p>
+            <p>{word}</p>
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={userInputs[index]} onChange={(event) => handleInputChange(event, index)} />
+              <button type="submit">Verificar</button>
+            </form>
+            {isExamCompleted && index === morseWords.length - 1 && (
+              <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Examen completado</ModalHeader>
+                  <ModalBody>
+                    ¡Respuestas correctas en todos los inputs!
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme="blue" onClick={handleCloseModal}>Cerrar</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
