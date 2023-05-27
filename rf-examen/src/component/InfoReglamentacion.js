e65e592940a281d3260ec8af4941ae65880f5475
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-05-27 11:41:41 -0300

    chakra-ui provider

--- rf-examen/src/component/InfoReglamentacion.js
+++ rf-examen/src/component/InfoReglamentacion.js
@@ -3,7 +3,6 @@ import facts from '../sources/facts-novicio-reglamentacion.json';
 import { Box, Button, Divider, Heading, Text } from '@chakra-ui/react';
 import './InfoReglamentacion.css';
 
-
 const InfoReglamentacion = () => {
   const handlePrint = () => {
     window.print();
@@ -15,14 +14,14 @@ const InfoReglamentacion = () => {
         <Heading paddingTop="50px" as="h1">Preguntas de examen: Categoría Novicio Reglamentación y Ética Operativa</Heading>
         <Button onClick={handlePrint}>Imprimir</Button> {/* Agregar botón de impresión */}
         {facts.map((fact, index) => (
-          <div key={fact.id}>
-          {index > 0 && <hr />} {/*   Separador visible */}
-          {/*  <Box key={fact.id} className="fact-row" border="1px solid" p="4" borderColor="gray.200">  */}
-            <p style={{ color: 'red', fontSize: '16px', fontWeight: 'bold' }}>{fact.id}</p> {/* Mostrar el ID */}
-            <h3>{fact.title}</h3>
-            <p style={{ color: 'green', fontSize: '16px', fontWeight: 'bold' }}>{fact.content}</p>
-          {/* </Box> */}
-          </div>
+          <Box key={fact.id} className="fact-row">
+            {index > 0 && <Divider />} {/* Separador visible */}
+            {/* <Box key={fact.id} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' p="4" borderColor="gray.200"> */}
+              <Text color="red" fontSize="16px" fontWeight="bold">{fact.id}</Text> {/* Mostrar el ID */}
+              <Heading as="h3">{fact.title}</Heading>
+              <Text color="green" fontSize="16px" fontWeight="bold">{fact.content}</Text>
+            {/* </Box> */}
+          </Box>
         ))}
       </Box>
     </Box>
